5488fa3851ca87d6126089bd4025f107240f58fa
Ryan4545 <51761134+Ryan4545@users.noreply.github.com>
2020-02-09 18:50:37 -0500

    Added main stuff

--- JS/index.js
+++ JS/index.js
@@ -7,12 +7,43 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const { ipcMain } = require('electron');
 
-MongoClient.connect(mongoUrl, function(err, db) {
-    if (err) throw err;
-    var dbo = db.db("Scouting");
-    let metrics = [];
-    let visability = []
-    dbo.collection("scouting-app").find().forEach(function(item) {
-        console.log(item);
+function getForData() {
+    document.getElementById('sortedDocs').innerHTML = "Loading Results...";
+    let data = document.getElementById('item').value;
+    MongoClient.connect(mongoUrl, function(err, db) {
+        if (err) throw err;
+        let teamNumbers = [];
+        var dbo = db.db("Scouting");
+        let teamData = dbo.collection("scouting-app").find().forEach(function(docs) {
+            teamNumbers.push((docs.teamNum).substring(3, 7));
+        })
+        setTimeout(function() {
+            for (let i = 0; i < teamNumbers.length; i++) {
+                if (data == teamNumbers[i]) {
+                    let query = {
+                        teamNum: `frc${teamNumbers[i]}`
+                    };
+                    let visability = []
+                    dbo.collection("scouting-app").find(query).forEach(function(item) {
+                        visability.push(item.metrics);
+                    });
+                    console.log(visability)
+                    setTimeout(function() {
+                        let delCells1 = 0;
+                        let pikCells = 0;
+                        let climb = 0;
+                        let visX = 0;
+                        console.log(visability.length)
+                        for (let x = 0; x < visability.length; x++) {
+                            pikCells = pikCells + visability[x].numberOfPickups;
+                            delCells1 = delCells1 + visability[x].deliveriesLvl1;
+                        }
+                        pikCells = pikCells / visability.length
+                        delCells1 = delCells1 / visability.length;
+                        document.getElementById('sortedDocs').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br> # of Pickups: ${pikCells} <br> # of Delivers to Level 1: ${delCells1}`;
+                    }, 800);
+                }
+            }
+        }, 800)
     });
-});
+}
