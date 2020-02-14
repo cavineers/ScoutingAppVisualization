e47e2361ee476b7f18b126bffa0e83b8bad46ada
Ryan4545 <51761134+Ryan4545@users.noreply.github.com>
2020-02-14 12:51:42 -0500

    Update format.js

--- format.js
+++ format.js
@@ -7,12 +7,11 @@ const dbfiles = [];
 
 fs.readdir(dbFolder, (err, files) => {
         if (err) throw (err)
-        console.log(files);
         files.forEach(file => {
             fs.readFile(__dirname + dbFolderFiles + file, 'utf8', (err, data) => {
                 if (err) throw err;
-                console.log(file)
                 dbfiles.push(file);
+                console.log(file);
                 fs.writeFile(`${file}.json`, data + '\n{"remove": 1}', function(err) {
                     if (err) throw (err);
                 })
@@ -23,19 +22,23 @@ fs.readdir(dbFolder, (err, files) => {
 var sourceDir = __dirname + '/db';
 var destDir = "E:\data(server)";
 
-// if folder doesn't exists create it
+/* if folder doesn't exists create it
 if (!fs.existsSync(destDir)) {
     fs.mkdirSync(destDir, { recursive: true });
-}
+} */
 
-//copy directory content including subfolders
-fse.copy(sourceDir, destDir, function(err) {
-    if (err) {
-        console.error(err);
-    } else {
-        console.log("success!");
-    }
-});
+if (!fs.existsSync(destDir)) {
+    console.log('Directory Not Found (Plug in Drive)')
+} else {
+    //copy directory content including subfolders
+    fse.copy(sourceDir, destDir, function(err) {
+        if (err) {
+            console.error(err);
+        } else {
+            console.log("success!");
+        }
+    });
+}
 setTimeout(function() {
     for (let i = 0; i < dbfiles.length; i++) {
         fs.unlinkSync(__dirname + dbFolderFiles + dbfiles[i]); //Deletes Files After being processed
