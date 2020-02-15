a6bd0c87954dfaa2823c9912a1a1230d069259c9
Ryan4545 <51761134+Ryan4545@users.noreply.github.com>
2020-02-14 20:12:15 -0500

    Added new metrics

--- dist/JS/index.js
+++ dist/JS/index.js
@@ -53,6 +53,8 @@ function getForData() {
                         let pins = 0;
                         let push = 0;
                         let disabled = 0;
+                        let stage2 = 0;
+                        let stage3 = 0;
                         let newData = 0; //newData = number of starting cells
                         for (let x = 0; x < visability.length; x++) {
                             pikCells = pikCells + visability[x].numberOfPickups;
@@ -66,6 +68,12 @@ function getForData() {
                             if (visability[x].climb.match(/YES/g)) {
                                 climb = climb + visability[x].climb.match(/YES/g).length || [];
                             }
+                            if (visability[x].stage2_control.match(/Spun_3-5_times/g)) {
+                                stage2 = stage2 + visability[x].stage2_control.match(/Spun_3-5_times/g).length || [];
+                            }
+                            if (visability[x].stage3_control.match(/LandedOnColor/g)) {
+                                stage3 = stage3 + visability[x].stage3_control.match(/LandedOnColor/g).length || [];
+                            }
                         }
                         pikCells = pikCells / visability.length
                         delCells1 = delCells1 / visability.length + newData;
@@ -76,7 +84,9 @@ function getForData() {
                         push = push / visability.length;
                         disabled = disabled / visability.length;
                         climb = (climb / visability.length) * 100;
-                        document.getElementById('sortedDocs').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: ${pikCells} <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
+                        stage2 = (stage2 / visability.length) * 100;
+                        stage3 = (stage3 / visability.length) * 100;
+                        document.getElementById('sortedDocs').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: ${pikCells} <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}%<br> avg percent of stage 2 (3-5): ${stage2}%<br> avg percent of landing on correct color: ${stage3}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
                         document.getElementById('loading').style.display = "none";
                     }, 1000);
                 } else {
@@ -131,6 +141,7 @@ function viewComments() {
 }
 
 function viewAllComments() {
+    document.getElementById('compare2').innerHTML = "";
     document.getElementById('rankingData').innerHTML = 'Loading Results...';
     let data = document.getElementById('item').value;
     MongoClient.connect(mongoUrl, function(err, db) {
@@ -168,6 +179,7 @@ function viewAllComments() {
 
 function getTeamNums() {
     document.getElementById('rankingData').innerHTML = 'Loading Results...';
+    document.getElementById('compare2').innerHTML = "";
     MongoClient.connect(mongoUrl, function(err, db) {
         if (err) throw err;
         let teamNumbers = [];
@@ -235,6 +247,8 @@ function allianceCompare() {
                         let pins = 0;
                         let push = 0;
                         let disabled = 0;
+                        let stage2 = 0;
+                        let stage3 = 0;
                         let newData = 0; //newData = number of starting cells
                         for (let x = 0; x < visability.length; x++) {
                             pikCells = pikCells + visability[x].numberOfPickups;
@@ -248,6 +262,12 @@ function allianceCompare() {
                             if (visability[x].climb.match(/YES/g)) {
                                 climb = climb + visability[x].climb.match(/YES/g).length || [];
                             }
+                            if (visability[x].stage2_control.match(/Spun_3-5_times/g)) {
+                                stage2 = stage2 + visability[x].stage2_control.match(/Spun_3-5_times/g).length || [];
+                            }
+                            if (visability[x].stage3_control.match(/LandedOnColor/g)) {
+                                stage3 = stage3 + visability[x].stage3_control.match(/LandedOnColor/g).length || [];
+                            }
                         }
                         pikCells = pikCells / visability.length
                         delCells1 = delCells1 / visability.length + newData;
@@ -258,6 +278,8 @@ function allianceCompare() {
                         push = push / visability.length;
                         disabled = disabled / visability.length;
                         climb = (climb / visability.length) * 100;
+                        stage2 = (stage2 / visability.length) * 100;
+                        stage3 = (stage3 / visability.length) * 100;
                         dataPos.push(teamNumbers[i]);
                         dataPos.push(newData);
                         dataPos.push(pikCells);
@@ -268,7 +290,7 @@ function allianceCompare() {
                         dataPos.push(pins);
                         dataPos.push(push);
                         dataPos.push(disabled);
-                        document.getElementById('rankingData').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: ${pikCells} <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
+                        document.getElementById('rankingData').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: ${pikCells} <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}%<br> avg percent of stage 2 (3-5): ${stage2}%<br> avg percent of landing on correct color: ${stage3}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
                         document.getElementById('loading2').style.display = "none";
                     }, 1000);
                 } else {
@@ -295,6 +317,8 @@ function allianceCompare() {
                         let pins = 0;
                         let push = 0;
                         let disabled = 0;
+                        let stage2 = 0;
+                        let stage3 = 0;
                         let newData = 0; //newData = number of starting cells
                         for (let x = 0; x < visability.length; x++) {
                             pikCells = pikCells + visability[x].numberOfPickups;
@@ -308,6 +332,12 @@ function allianceCompare() {
                             if (visability[x].climb.match(/YES/g)) {
                                 climb = climb + visability[x].climb.match(/YES/g).length || [];
                             }
+                            if (visability[x].stage2_control.match(/Spun_3-5_times/g)) {
+                                stage2 = stage2 + visability[x].stage2_control.match(/Spun_3-5_times/g).length || [];
+                            }
+                            if (visability[x].stage3_control.match(/LandedOnColor/g)) {
+                                stage3 = stage3 + visability[x].stage3_control.match(/LandedOnColor/g).length || [];
+                            }
                         }
                         pikCells = pikCells / visability.length
                         delCells1 = delCells1 / visability.length + newData;
@@ -318,16 +348,88 @@ function allianceCompare() {
                         push = push / visability.length;
                         disabled = disabled / visability.length;
                         climb = (climb / visability.length) * 100;
+                        stage2 = (stage2 / visability.length) * 100;
+                        stage3 = (stage3 / visability.length) * 100;
                         /* if (dataPos[2] > pikCells) {
                              document.getElementById('compare2').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: <span style="color: red;">${pikCells}</span> <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
                              document.getElementById('rankingData').innerHTML = ` Average Stats for Team ${dataPos[0]}: <br><br>Metrics:<br> # of starting cells: ${dataPos[1]} <br> # of Pickups: <span style="color: green;">${dataPos[2]}</span> <br> # of Delivers to Level 1: ${dataPos[3]} <br> # of Delivers to Level 2: ${dataPos[4]} <br> # of Delivers to Level 3: ${dataPos[5]} <br> avg percent of climbs: ${dataPos[6]}% <br><br> Defense: <br> # of Pins ${dataPos[7]} <br> # of Pushes ${dataPos[8]} <br> # of Disables ${dataPos[9]}`;
                          } else {
                              document.getElementById('compare2').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: <span style="color: green;">${pikCells}</span> <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
                          } */
-                        document.getElementById('compare2').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: ${pikCells} <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
+                        document.getElementById('compare2').innerHTML = ` Average Stats for Team ${teamNumbers[i]}: <br><br>Metrics:<br> # of starting cells: ${newData} <br> # of Pickups: ${pikCells} <br> # of Delivers to Level 1: ${delCells1} <br> # of Delivers to Level 2: ${delCells2} <br> # of Delivers to Level 3: ${delCells3} <br> avg percent of climbs: ${climb}%<br> avg percent of stage 2 (3-5): ${stage2}%<br> avg percent of landing on correct color: ${stage3}% <br><br> Defense: <br> # of Pins ${pins} <br> # of Pushes ${push} <br> # of Disables ${disabled}`;
                     }, 1000);
                 }
             }
         }, 1000)
     });
+}
+
+function baseLine() {
+    document.getElementById('loading2').style.display = "block";
+    document.getElementById('rankingData').innerHTML = "Parsing Data...";
+    document.getElementById('compare2').innerHTML = "";
+    MongoClient.connect(mongoUrl, function(err, db) {
+        if (err) throw err;
+        let pikCells = [];
+        let delCells1 = [];
+        let delCells2 = [];
+        let delCells3 = [];
+        let climb = 0;
+        let pins = [];
+        let push = [];
+        let disabled = [];
+        let stage2 = 0;
+        let stage3 = 0;
+        var dbo = db.db("Scouting");
+        let teamData = dbo.collection(collectionName).find().forEach(function(docs) {
+            pikCells.push(docs.metrics.numberOfPickups);
+            delCells1.push(docs.metrics.deliveriesLvl1);
+            delCells2.push(docs.metrics.deliveriesLvl2);
+            delCells3.push(docs.metrics.deliveriesLvl3);
+            if (docs.metrics.climb.match(/YES/g)) {
+                climb = climb + docs.metrics.climb.match(/YES/g).length || [];
+            }
+            if (docs.metrics.stage2_control.match(/Spun_3-5_times/g)) {
+                stage2 = stage2 + docs.metrics.stage2_control.match(/Spun_3-5_times/g).length || [];
+            }
+            if (docs.metrics.stage3_control.match(/LandedOnColor/g)) {
+                stage3 = stage3 + docs.metrics.stage3_control.match(/LandedOnColor/g).length || [];
+            }
+            pins.push(docs.metrics.numPins);
+            push.push(docs.metrics.numPush);
+            disabled.push(docs.metrics.numDisrupted);
+        });
+        setTimeout(function() {
+            let pikCellsParse = 0;
+            let delCells1Parse = 0;
+            let delCells2Parse = 0;
+            let delCells3Parse = 0;
+            let pinsParse = 0;
+            let pushParse = 0;
+            let disabledParse = 0;
+            let length = 0;
+            for (let i = 0; i < pikCells.length; i++) {
+                pikCellsParse = pikCellsParse + pikCells[i];
+                delCells1Parse = delCells1Parse + delCells1[i];
+                delCells2Parse = delCells2Parse + delCells2[i];
+                delCells3Parse = delCells3Parse + delCells3[i];
+                pinsParse = pinsParse + pins[i];
+                pushParse = pushParse + push[i];
+                disabledParse = disabledParse + disabled[i];
+                length = i + 1;
+            }
+            pikCellsParse = pikCellsParse / length;
+            delCells1Parse = delCells1Parse / length;
+            delCells2Parse = delCells2Parse / length;
+            delCells3Parse = delCells3Parse / length;
+            pinsParse = pinsParse / length;
+            pushParse = pushParse / length;
+            disabledParse = disabledParse / length;
+            climb = (climb / length) * 100;
+            stage2 = (stage2 / length) * 100;
+            stage3 = (stage3 / length) * 100;
+            document.getElementById('rankingData').innerHTML = `<span style="font-size: 24px;">Average Pick Ups: <span style="color: yellow;">${pikCellsParse}</span><br>Average Delivers to Level 1: <span style="color: yellow;">${delCells1Parse}</span><br>Average Delivers to Level 2: <span style="color: yellow;">${delCells2Parse}</span><br>Average Delivers to Level 3: <span style="color: yellow;">${delCells3Parse}</span><br>Average Stage 2 Spin (3-5): <span style="color: yellow;">${stage2}%</span><br>Average Lands on Correct Color: <span style="color: yellow">${stage3}%</span><br>Average Climb: <span style="color: yellow;">${climb}%</span><br>Average Pins: <span style="color: yellow;">${pinsParse}</span><br>Average Push: <span style="color: rgb(3, 223, 252);">${pushParse}</span><br>Average Disables: <span style="color: rgb(3, 223, 252);">${disabledParse}</span></span>`;
+            document.getElementById('loading2').style.display = "none";
+        }, 900);
+    })
 }
